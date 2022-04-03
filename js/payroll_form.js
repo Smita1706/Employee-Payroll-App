0342b65ca586d31ad3f07ef0060b238783dd43ba
Smita1706 <smitachinchkar1998@gmail.com>
2022-04-03 23:33:55 +0530

    Added The use case 4 Ability to specify the gender

---
+++ js/payroll_form.js
@@ -0,0 +1,6 @@
+
+const salary = document.querySelector("#salary");
+const output = document.querySelector(".salary-output");
+salary.oninput = function() {
+    output.textContent = salary.value;
+};
